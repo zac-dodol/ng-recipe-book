e1a3ffcfa7bbf6ac1367459376cea3092fde7031
zac-dodol <akmalhazimothman@hotmail.com>
2023-11-06 17:27:27 +0800

    adding to shopping list

--- ng-study/src/app/component/shopping-list/shopping-list.component.ts
+++ ng-study/src/app/component/shopping-list/shopping-list.component.ts
@@ -11,4 +11,12 @@ export class ShoppingListComponent {
     new Ingredient('Apples', 5),
     new Ingredient('Salad', 10),
   ];
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+
+  onIngredientDelete() {
+    this.ingredients.pop();
+  }
 }
