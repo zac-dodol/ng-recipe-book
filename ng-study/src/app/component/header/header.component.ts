20474c288eef34ab021148d6a048549ef9abf243
zac-dodol <akmalhazimothman@hotmail.com>
2023-11-07 16:58:46 +0800

    layout restructuring

--- ng-study/src/app/component/header/header.component.ts
+++ ng-study/src/app/component/header/header.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private router: Router) {}
+
+  get isHomeRoute(): boolean {
+    return this.router.url === '/';
+  }
+
+  get isRecipeRoute(): boolean {
+    return this.router.url === '/recipe-page';
+  }
+}
